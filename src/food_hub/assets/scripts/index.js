079fb17a12317decbc411d417dcb00435c9e8392
SMassalski <stanislaw.massalski@gmail.com>
2023-01-19 15:33:19 +0100

    DEV: Sass setup
    Added sass to frontend dependencies for bootstrap
    theming. Sass file is compiled to bundle.css in the
    static folder.
    
    Adjusted the base template accordingly and to
    match bootstrap recommendation.
    
    Added a theme and a chart palette.

--- src/food_hub/assets/scripts/index.js
+++ src/food_hub/assets/scripts/index.js
@@ -2,4 +2,5 @@ import 'htmx.org';
 import 'admin-lte';
 import Chart from 'chart.js/auto'; // needs to be optimized
 
+
 window.Chart = Chart;
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
 const path = require('path');
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
 
 module.exports = {
     mode: "production",
-    entry: './src/food_hub/assets/index.js',
+    entry: ['./src/food_hub/assets/scripts/index.js', './src/food_hub/assets/style/style.scss'],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, './src/food_hub/static'),
+        library: "lib"
     },
-    plugins: [new BundleAnalyzerPlugin()],
+    module : {
+        rules : [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [],
+            },
+            {
+                test: /\.s[ac]ss$/,
+                exclude: /node_modules/,
+                type: "asset/resource",
+                generator: {
+                    filename: "bundle.css"
+                },
+                use: ["sass-loader"],
+            },
+        ],
+    },
+
 };
