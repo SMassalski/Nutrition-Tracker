f65c674a30bb14e68e9814663f4de95a61b0027e
SMassalski <stanislaw.massalski@gmail.com>
2023-02-02 14:57:26 +0100

    ENH: Static Dashboard templates
    Added dashboard template view to url conf.
    
    Static templates added:
    * Dashboard page
    * Dashboard header card and associated modals:
      - Tracked Nutrients - Display / Add / Remove
      tracked nutrients. Tracked nutrients will have
      their own chart in the dashboard and be
      highlighted in detailed views.
      - Modify diet - Add / Edit / Remove a diet
      modification that changes daily intakes.
      - Personal info - Edit personal information that
      is used for calculating recommended calorie
      intake.
      + Base modal template

--- src/food_hub/main/urls.py
+++ src/food_hub/main/urls.py
@@ -1,10 +1,11 @@
 """main app URL Configuration"""
 from django.urls import path
 from django.views.generic import TemplateView
-from main.views.main import home
 
 urlpatterns = [
-    path("", home, name="dashboard"),
+    path(
+        "", TemplateView.as_view(template_name="main/dashboard.html"), name="dashboard"
+    ),
     path(
         "new_meal/",
         TemplateView.as_view(template_name="main/compose_meal.html"),
