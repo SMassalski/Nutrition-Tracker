81f3d79c3c6f24370c593df382faefd994897da1
SMassalski <stanislaw.massalski@gmail.com>
2023-02-20 16:55:16 +0100

    FIX: Black pie chart bug
    The chart_palette CSS property was incorrectly read.
    Properties are read as strings hence array properties
    require .split().
    
    + New chart CSS properties:
      - chart_primary
      - chart_annotation

--- src/food_hub/static/charts_wrapper.js
+++ src/food_hub/static/charts_wrapper.js
@@ -1,16 +1,12 @@
-const palette = getComputedStyle(document.documentElement)
-    .getPropertyValue("--chart_palette") || ["#33658A", "#86BBD8", "#2B8C69", "#F6AE2D", "#F26419"];
+/**
+ * Chart wrappers for easy themed charts.
+ */
+// TODO: Length independent color palette
+const style = getComputedStyle(document.documentElement)
+const palette = style.getPropertyValue("--chart_palette").split(", ") || ["#33658A", "#86BBD8", "#2B8C69", "#F6AE2D", "#F26419"];
+const primary = style.getPropertyValue("--chart_primary") || palette[0] || "#86BBD8";
+const annotation_color = style.getPropertyValue("--chart_annotation") || palette[palette.length - 1] || "#F26419";
 
-/*
-const rangedChart = function(elementId, data, lower, upper) {
-    const chart = new Chart (document.getElementById(elementId), {
-        type: 'bar',
-        data: {
-
-        }
-    });
-};
-*/
 /**
  * Draw a chart.js pie chart in a canvas element
  * @param {string} elementId - The id of the canvas.
