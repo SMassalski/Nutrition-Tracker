ab1eba7874b6c1073172b05cf99b7afbce251e89
SMassalski <stanislaw.massalski@gmail.com>
2023-04-04 10:24:48 +0200

    ENH: Profile form unit conversion
    Profile form now allows users to select the unit used
    in weight input and has fields for height that
    automatically perform unit conversion between cm and
    ft/in. JS input event listeners were added to height
    fields in profile.html to perform conversions
    client-side.
    The form's layout is defined using crispy forms.

--- src/food_hub/main/forms.py
+++ src/food_hub/main/forms.py
@@ -1,19 +1,125 @@
 """Main app forms."""
+from typing import Tuple
+
+from crispy_forms import bootstrap, layout
+from crispy_forms.helper import FormHelper
 from django import forms
 from main.models import Profile
-
-# TODO: Unit selection and conversion for weight and height
 
 
 class ProfileForm(forms.ModelForm):
     """Form for creating and editing user profiles."""
+
+    # Weight fields
+    POUNDS = 1
+    KILOGRAMS = 2
+    weight_unit_choices = [(POUNDS, "lbs"), (KILOGRAMS, "kg")]
+    weight = forms.IntegerField(label="")
+    weight_unit = forms.ChoiceField(
+        choices=weight_unit_choices, label="", initial=KILOGRAMS, required=False
+    )
+
+    # Height fields
+    # Only the `height` field is considered when saving the profile.
+    # Unit conversions between cm and ft/in are performed client-side
+    # (js script is in the profile.html template).
+    height = forms.IntegerField(label="")
+    feet = forms.IntegerField(label="", required=False)
+    inches = forms.IntegerField(label="", required=False)
 
     class Meta:
         model = Profile
         fields = [
             "age",
             "height",
+            "feet",
+            "inches",
             "weight",
+            "weight_unit",
             "sex",
             "activity_level",
         ]
+
+    def __init__(self, *args, **kwargs):
+        """ProfileForm constructor.
+
+        Defines layout using crispy-forms and includes initial unit
+        conversion for height (Stored in database in cm, displayed in
+        both cm and ft/in).
+        See ModelForm documentation for additional information.
+        """
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+
+        self.helper.layout = layout.Layout(
+            "age",
+            layout.Div(
+                layout.HTML(
+                    '<label class="requiredField" for="id_height">Height'
+                    '<span class="asteriskField">*</span></label>'
+                ),
+                # DEV NOTE: Having two different fieldsets for different
+                #  units requires JS code for automatic conversions
+                #  for adequate user experience.
+                layout.Row(
+                    layout.Column(bootstrap.AppendedText("height", "cm")),
+                    layout.Column(
+                        layout.HTML(
+                            '<span class="d-flex align-items-center form-group">'
+                            "or"
+                            "</span>"
+                        ),
+                        css_class="col-1 d-flex justify-content-center",
+                    ),
+                    layout.Column(bootstrap.AppendedText("feet", "ft")),
+                    layout.Column(bootstrap.AppendedText("inches", "in")),
+                ),
+                css_class="form-group mb-0",
+            ),
+            layout.Div(
+                layout.HTML(
+                    '<label class="requiredField" for="id_weight">Weight'
+                    '<span class="asteriskField">*</span></label>'
+                ),
+                layout.Row(
+                    layout.Column("weight"),
+                    layout.Column("weight_unit", css_class="col-2"),
+                ),
+                css_class="form-group mb-0",
+            ),
+            "sex",
+            "activity_level",
+            layout.Submit("submit", "Save", style="width: 100%"),
+        )
+
+        # Initial height unit conversion
+        feet, inches = centimeters_to_feet_and_inches(self.instance.height or 0)
+        self["feet"].initial, self["inches"].initial = feet, inches
+
+        # Initial weight unit because value is stored in kg and
+        # displayed without conversion.
+        # self["weight_unit"].initial = ProfileForm.KILOGRAMS
+
+    def clean(self):
+        """Clean weight data by converting the value to kilograms."""
+        cleaned_data = super().clean()
+        weight = cleaned_data.get("weight")
+        # cleaned_data.get() returns '' if field was not provided.
+        weight_unit = cleaned_data.get("weight_unit") or ProfileForm.KILOGRAMS
+
+        if int(weight_unit) == ProfileForm.POUNDS:
+            cleaned_data["weight"] = pounds_to_kilograms(weight)
+
+        return cleaned_data
+
+
+def pounds_to_kilograms(weight: int) -> int:
+    """Convert the given weight in pounds to kilograms."""
+    return round(weight * 0.454)
+
+
+def centimeters_to_feet_and_inches(centimeters: int) -> Tuple[int, int]:
+    """Convert the given length in feet and inches to centimeters."""
+    feet = int(centimeters // 30.48)
+    inches = round((centimeters / 30.48 - feet) * 12)
+    return feet, inches
---
+++ tests/test_forms.py
@@ -0,0 +1,72 @@
+"""Tests of main app's forms and form utility functions."""
+from main import forms
+from main.models import Profile
+
+
+def test_profile_form_initial_height_unit_conversion():
+    """
+    ProfileForm converts height value to both cm and ft/in after
+    instantiation.
+    """
+    profile = Profile(height=180)
+    form = forms.ProfileForm(instance=profile)
+    assert form["height"].value() == 180
+    assert form["feet"].value() == 5
+    assert form["inches"].value() == 11
+
+
+def test_profile_form_weight_cleaning_unit_conversion():
+    """
+    ProfileForm converts weight value to kilograms during data cleaning
+    if 'weight_unit' was set to pounds.
+    """
+    form = forms.ProfileForm(
+        {
+            "age": 20,
+            "sex": "M",
+            "activity_level": "A",
+            "height": 178,
+            "weight": 176,
+            "weight_unit": forms.ProfileForm.POUNDS,
+        }
+    )
+    assert form.is_valid()
+    assert form.cleaned_data["weight"] == 80
+    profile = form.save(commit=False)
+    assert profile.weight == 80
+
+
+def test_profile_form_weight_cleaning_no_unit_conversion_when_kg_used():
+    """
+    ProfileForm doesn't convert weight value if 'weight_unit' was set to
+    kilograms.
+    """
+    form = forms.ProfileForm(
+        {
+            "age": 20,
+            "sex": "M",
+            "activity_level": "A",
+            "height": 178,
+            "weight": 80,
+            "weight_unit": forms.ProfileForm.KILOGRAMS,
+        }
+    )
+    form.is_valid()
+    assert form.cleaned_data["weight"] == 80
+
+
+def test_pounds_to_kilograms():
+    """
+    pounds_to_kilograms() correctly converts a weight in pounds to a
+    weight in kilograms (with rounding).
+    """
+    assert forms.pounds_to_kilograms(2) == 1
+    assert forms.pounds_to_kilograms(3) == 1
+
+
+def test_centimeters_to_feet_and_inches():
+    """
+    centimeters_to_feet_and_inches() correctly converts a height in
+    centimeters to a height in feet and inches (with rounding).
+    """
+    assert forms.centimeters_to_feet_and_inches(33) == (1, 1)
