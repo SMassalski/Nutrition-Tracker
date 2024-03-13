375b77897562a884949970dd53f46918861e78e4
SMassalski <stanislaw.massalski@gmail.com>
2024-03-13 16:36:36 +0100

    ENH: Make colors in the macros chart consistent
    The colors in the chart are now set based on nutrient name.
    Supports "Alcohol", "Carbohydrate", "Lipid" and "Protein"
    
    + rename the `pieChart` function to `MacrosPieChart`.

--- src/food_hub/static/charts_wrapper.js
+++ src/food_hub/static/charts_wrapper.js
@@ -6,24 +6,36 @@ const style = getComputedStyle(document.documentElement)
 const palette = style.getPropertyValue("--chart_palette").split(", ") || ["#33658A", "#86BBD8", "#2B8C69", "#F6AE2D", "#F26419"];
 const primary = style.getPropertyValue("--chart_primary") || palette[0] || "#86BBD8";
 const annotation_color = style.getPropertyValue("--chart_annotation") || palette[palette.length - 1] || "#F26419";
+const colorMap = {
+      "Lipid": palette[3],
+      "Protein": palette[0],
+      "Carbohydrate": palette[1],
+      "Alcohol": palette[2]
+}
 
 /**
  * Draw a chart.js pie chart in a canvas element
  * @param {string} elementId - The id of the canvas.
  * @param {Object} data - Mapping in the format of {<label>: <value>}.
  */
-const pieChart = function(elementId, data) {
+const MacrosPieChart = function(elementId, data) {
+
+    labels = Object.keys(data)
+    colors = [];
+    for (i=0; i < labels.length; i++) {
+        colors[i] = colorMap[labels[i]];
+    }
 
     const ctx = document.getElementById(elementId);
 
     new Chart(ctx, {
         type: 'pie',
         data: {
-            labels: Object.keys(data),
+            labels: labels,
             datasets: [
                 {
                     data: Object.values(data),
-                    backgroundColor: palette
+                    backgroundColor: colors
                 }
             ]
         },
