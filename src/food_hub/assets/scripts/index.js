853d85cc127761698e4e649cd00f0b617355fe6c
SMassalski <stanislaw.massalski@gmail.com>
2024-04-23 14:52:08 +0200

    MAINT: Tree shake bootstrap JS

--- src/food_hub/assets/scripts/index.js
+++ src/food_hub/assets/scripts/index.js
@@ -1,5 +1,18 @@
 import "jquery";
-import 'bootstrap';
+
+// Bootstrap
+import 'bootstrap/js/dist/alert';
+import 'bootstrap/js/dist/button';
+// import 'bootstrap/js/dist/carousel';
+// import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/popover';
+// import 'bootstrap/js/dist/scrollspy';
+import 'bootstrap/js/dist/tab';
+// import 'bootstrap/js/dist/toast';
+import 'bootstrap/js/dist/tooltip';
+
 import * as charts from './chart_util.js';
 import * as util from './util.js'
 
