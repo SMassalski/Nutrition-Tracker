a560ffb6decf5580c593fe7a01373c834be1f647
SMassalski <stanislaw.massalski@gmail.com>
2024-04-24 12:30:22 +0200

    FIX: Chart theme colors
    Minification of css removed spaces in the definition of the
    `--chart_palette` property. The spaces were taken into account when
    retrieving the value and splitting it into an array. Removal of those
    spaces caused incorrect split.

--- src/food_hub/assets/scripts/chart_util.js
+++ src/food_hub/assets/scripts/chart_util.js
@@ -36,7 +36,7 @@ Chart.register(
 
 
 const style = getComputedStyle(document.documentElement)
-const palette = style.getPropertyValue("--chart_palette").split(", ") || ["#33658A", "#86BBD8", "#2B8C69", "#F6AE2D", "#F26419"];
+const palette = style.getPropertyValue("--chart_palette").split(",") || ["#33658A", "#86BBD8", "#2B8C69", "#F6AE2D", "#F26419"];
 const [primary, info, success, warning, danger, ...rest] = palette;
 const chartPrimary = style.getPropertyValue("--chart_primary") || palette[1] || "#86BBD8";
 const annotation_color = style.getPropertyValue("--chart_annotation") || palette[palette.length - 1] || "#F26419";
